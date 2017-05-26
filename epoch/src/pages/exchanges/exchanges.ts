1b9f43fc85d92805bb9f8c09ab14b1a18421f99c
jajsmith <j.jaydez@gmail.com>
2017-05-26 10:10:33 -0400

    Feature: Add requests to database using alert on requests list page

--- epoch/src/pages/exchanges/exchanges.ts
+++ epoch/src/pages/exchanges/exchanges.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 import {AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database';
 
@@ -11,8 +11,53 @@ import {AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database
 export class ExchangesPage {
   requests: FirebaseListObservable<any[]>;
 
-  constructor(public navCtrl: NavController, db: AngularFireDatabase) {
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController, db: AngularFireDatabase) {
     this.requests = db.list('/sample/requests');
+  }
+
+  addRequest() {
+    var d = new Date();
+    let prompt = this.alertCtrl.create({
+      title: 'Request Name',
+      message: "Enter the title of your new request",
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'Title'
+        },
+        {
+          name: 'description',
+          placeholder: 'Request description'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            this.requests.push({
+              title: data.title,
+              description: data.description,
+              starttime: d.getTime(),
+              duration: 2,
+              creator: "user_id(DreamTeam)",
+              status: "open"
+            });
+            // TODO: Add this request to the user's list of requests
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+  showOptions(requestId) {
+    // TODO: Direct to request-detail page
   }
 
 }
