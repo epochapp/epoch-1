ac579b57619b5960ea5b680f72a47397605a7484
jajsmith <j.jaydez@gmail.com>
2017-05-26 10:27:50 -0400

    Minimal request editing and deletion features

--- epoch/src/pages/exchanges/exchanges.ts
+++ epoch/src/pages/exchanges/exchanges.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, AlertController } from 'ionic-angular';
+import { NavController, AlertController, ActionSheetController } from 'ionic-angular';
 
 import {AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database';
 
@@ -11,10 +11,14 @@ import {AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database
 export class ExchangesPage {
   requests: FirebaseListObservable<any[]>;
 
-  constructor(public navCtrl: NavController, public alertCtrl: AlertController, db: AngularFireDatabase) {
+  constructor(public navCtrl: NavController, 
+              public alertCtrl: AlertController, 
+              public actionSheetCtrl: ActionSheetController, 
+              db: AngularFireDatabase) {
     this.requests = db.list('/sample/requests');
   }
 
+  // Generates a new request entity in Firebase using user input
   addRequest() {
     var d = new Date();
     let prompt = this.alertCtrl.create({
@@ -56,8 +60,96 @@ export class ExchangesPage {
     prompt.present();
   }
 
-  showOptions(requestId) {
+  // Shows actions that can be taken to respond to or delete requests
+  showOptions(requestId, requestDescription) {
     // TODO: Direct to request-detail page
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'What do you want to do?',
+      buttons: [
+        {
+          text: 'Offer Assistance',
+          handler: () => {
+            this.respondToRequest(requestId);
+          }
+        },{
+          text: 'Update Description',
+          handler: () => {
+            this.updateRequest(requestId, requestDescription);
+          }
+        },{
+          text: 'Delete Request',
+          role: 'destructive',
+          handler: () => {
+            this.removeRequest(requestId);
+          }
+        },{
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
   }
 
+  removeRequest(requestId: string){
+    this.requests.remove(requestId);
+  }
+
+  updateRequest(requestId: string, requestDescription: string){
+    let prompt = this.alertCtrl.create({
+      title: 'Request Description',
+      message: "Update the description for this request",
+      inputs: [
+        {
+          name: 'description',
+          placeholder: 'Description',
+          value: requestDescription
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            this.requests.update(requestId, {
+              description: data.description
+            });
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+  respondToRequest(requestId: string){
+    let prompt = this.alertCtrl.create({
+      title: 'Respond to Request',
+      message: "Will you offer to fulfill this request?",
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Confirm',
+          handler: data => {
+            this.requests.update(requestId, {
+              status: "closed"
+            });
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
 }
