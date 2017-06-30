502d073f891e6c160919eb8b22da6b5de9a7bc28
Rajat Goswami <rajat.goswami@hotmail.com>
2017-06-29 23:19:10 -0400

    Allow for plain number as input

--- epoch/src/validators/tel.ts
+++ epoch/src/validators/tel.ts
@@ -5,6 +5,7 @@ export class TelValidator {
   static isValid(control: FormControl) {
     // Telephone number regular expression 
     /* Matches the following:
+        1234567890
         123-456-7890
         (123) 456-7890
         123 456 7890
@@ -12,7 +13,7 @@ export class TelValidator {
         +91 (123) 456-7890 
     Source: https://stackoverflow.com/questions/16699007/regular-expression-to-match-standard-10-digit-phone-number*/
 
-    const telValidationRegEx = /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/.test(control.value);
+    const telValidationRegEx = /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/.test(control.value);
 
     if (telValidationRegEx) {
       return null;
